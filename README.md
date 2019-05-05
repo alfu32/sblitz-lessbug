92eb0bb52a80480699e08e11688d28e3e3fc2bc7
alfu32 <ioan.alferaru@gmail.com>
2019-05-05 08:05:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-b6yavi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-b6yavi)
---
+++ index.js
@@ -0,0 +1,11 @@
+// Import stylesheets
+import './style.sass';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter
+<div class="one">
+  <div class="two">two</div>
+  <div class="three">three</div>
+</div>
+</h1>`;
